8496c9975163e77f5b03eac9c6b84cb967a11e47
dicky Ardianto <dikiardianto19@gmail.com>
2019-10-06 20:42:07 +0700

    menambahkan baris ke 13 dan 14

--- app/core/App.php
+++ app/core/App.php
@@ -10,6 +10,8 @@ Class App {
     public function parseURL() {
         if(isset($_GET['url'])) {
             $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
             return $url;
         }
     }
