bce131ab6227221c74678e489869718e3bf26ff9
dicky Ardianto <dikiardianto19@gmail.com>
2019-10-08 15:28:26 +0700

    Memodifikasi file About.php, home.php, Controllers.php, dan menambahkan fitur index.php->about, page.php->about, index.php->home, serta footer.php, header.php->template

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,11 +1,20 @@
 <?php
 
-class About {
-    public function index($nama = 'Dicky', $pekerjaan = 'Mahasiswa') {
-        echo "Hallo, nama saya $nama, saya adalah $pekerjaan";
+class About extends Controllers {
+    public function index($nama = 'Dicky', $pekerjaan = 'Mahasiswa' , $umur = 32) {
+        $data['nama'] = $nama;
+        $data['pekerjaan'] = $pekerjaan;
+        $data['umur'] = $umur;
+        $data['judul'] = 'About Me';
+        $this->view('template/header', $data);
+        $this->view('about/index' , $data);
+        $this->view('template/footer');
     }
 
     public function page() {
-        echo "About";
+        $data['judul'] = 'Page';
+        $this->view('template/header', $data);
+        $this->view('about/page');
+        $this->view('template/footer');
     }
 }
---
+++ app/views/about/index.php
@@ -0,0 +1,2 @@
+<h1>Ini Halaman About</h1>
+<p>Hallo Nama saya <?php echo $data['nama']; ?>, Umur saya <?php echo $data['umur'] . ","; ?> Saya adalah seorang <?php echo $data['pekerjaan']; ?></p>
